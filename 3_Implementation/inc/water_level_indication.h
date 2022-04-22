ea4e14d390b417f9641fe402eba48f2ab1457e6c
Faadil Batcha <101641134+Faadilbatcha@users.noreply.github.com>
2022-04-22 12:23:22 +0530

    Update water_level_indication.h

--- 3_Implementation/inc/water_level_indication.h
+++ 3_Implementation/inc/water_level_indication.h
@@ -1,8 +1,29 @@
+/**
+ * @file water_level_indication.h
+ * @author Faadil Batcha
+ * @brief Header file for Water Level Indication with Ultrasonic & Bluetooth sensor operations
+ *
+ */
+
 #ifndef WATER_LEVEL_INDICATION_H_INCLUDED
 #define WATER_LEVEL_INDICATION_H_INCLUDED
 
+/**
+* @brief Initialize USART & enable transmitter and receiver
+* @param[in] uint16_t 
+*/
 void USARTInit(uint16_t );
+
+/**
+* @brief helps to read a char from the PC
+* @return UDR0
+*/
 char USARTReadChar();
-void USARTWriteChar(char );
+
+/**
+* @brief helps to write a char/sentence to the PC
+* @param[in] char *
+*/
+void USARTWriteChar(char *;
 
 #endif // WATER_LEVEL_INDICATION_H_INCLUDED
